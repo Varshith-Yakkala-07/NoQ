f32ed4e4f7010e03a596a3e6e833b532af54c30a
Varshith Yakkala <rampushpavarshithyakkala@gmail.com>
2026-04-18 20:06:11 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js"
+import { connectDB } from "./lib/db.js"
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+//Routes
+app.use("/api/auth", authRoutes);
+
+connectDB().then(() => {
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`); 
+  });
+});
+
+
