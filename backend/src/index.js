e6b376f257b5e2b1746bdad05936612ae63dd201
Varshith Yakkala <rampushpavarshithyakkala@gmail.com>
2026-04-18 22:41:41 +0530

    cron.js added

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,12 @@ import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js"
 import { connectDB } from "./lib/db.js"
 import cors from "cors";
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
