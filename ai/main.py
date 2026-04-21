a56b605b49f29c56c4edfc460d44e6916121ce93
Varshith Yakkala <rampushpavarshithyakkala@gmail.com>
2026-04-21 23:28:35 +0530

    test 1

--- ai/main.py
+++ ai/main.py
@@ -1,71 +1,3 @@
-import express from "express";
-import axios from "axios";
-
-const PYTHON_URL = "https://smog-baboon-gloomily.ngrok-free.dev";
-const router = express.Router();
-
-router.get("/crowd1", async (req, res) => {
-  try {
-    const response = await axios.get(`${PYTHON_URL}/data1`);
-
-    res.json(response.data); // forward Python response
-
-  } catch (error) {
-    console.error("Error calling Python API:", error.message);
-    res.status(500).json({ error: "Python service failed" });
-  }
-});
-
-router.get("/crowd2", async (req, res) => {
-  try {
-    const response = await axios.get(`${PYTHON_URL}/data2`);
-
-    res.json(response.data); // forward Python response
-
-  } catch (error) {
-    console.log("error");
-    console.error("Error calling Python API:", error.message);
-    res.status(500).json({ error: "Python service failed" });
-  }
-});
-router.get("/crowd3", async (req, res) => {
-  try {
-    const response = await axios.get(`${PYTHON_URL}/data3`);
-
-    res.json(response.data); // forward Python response
-
-  } catch (error) {
-    console.error("Error calling Python API:", error.message);
-    res.status(500).json({ error: "Python service failed" });
-  }
-});
-
-router.get("/crowd4", async (req, res) => {
-  try {
-    const response = await axios.get(`${PYTHON_URL}/data4`);
-
-    res.json(response.data);
-
-  } catch (error) {
-    console.error("Error calling Python API:", error.message);
-    res.status(500).json({ error: "Python service failed" });
-  }
-});
-export default router;
-
-
-
-
-
-
-
-
-
-
-
-
-/* Python code AI 
-
 import cv2
 import torch
 from ultralytics import YOLO
@@ -185,4 +117,3 @@ def hall4():
 
 
 
-*/
